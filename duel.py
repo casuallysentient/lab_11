95635591d147e141bb55f0a1592dc14b6ec858bb
Reilly <reilly.ford@gmail.com>
2021-05-01 12:48:17 -0400

    2.3

--- duel.py
+++ duel.py
@@ -23,47 +23,50 @@ class Duelist:
     def __str__(self):
         return "Duelist object '{duelist_name}', carrying {weapon_inventory[0].weapon_name}, {weapon_inventory[1].weapon_name}, and {weapon_inventory[2].weapon_name} Weapon objects.".format(duelist_name=self.duelist_name, weapon_inventory=self.weapon_inventory)
 
+    def get_winner_of_duel_name(self, opponent):
+        self_weapon = self.weapon_inventory[random.randrange(0, 3)]
+        opponent_weapon = opponent.weapon_inventory[random.randrange(0, 3)]
+        print("Duelist {self_name} picked a {self_weapon.weapon_name}!".format(self_name=self.duelist_name, self_weapon=self_weapon))
+        print("Duelist {opponent_name} picked a {opponent_weapon.weapon_name}!".format(opponent_name=opponent.duelist_name, opponent_weapon=opponent_weapon))
+        if self_weapon.strength > opponent_weapon.strength:
+            if self_weapon.does_break():
+                return self.duelist_name
+            return opponent.duelist_name
+        elif self_weapon.strength < opponent_weapon.strength:
+            if opponent_weapon.does_break():
+                return opponent.duelist_name
+            return self.duelist_name
+        elif self_weapon.strength == opponent_weapon.strength:
+            print("Both duelists picked weapons of the same strength! The winner will be decided purely by pseudo-randomly generated fate!")
+            coin_flip = random.randrange(0,2)
+            if coin_flip == 0:
+                return self.duelist_name
+            return opponent.duelist_name
+
 
 def main():
     """
     Sample behavior based on the README.
     """
     # Creating my Weapon objects
-    test_weapon = Weapon('naginata', 0.75)
-    weapon_1 = Weapon("Rickenbacker 4001c64", 0.6)
+    weapon_1 = Weapon("Rickenbacker 4001c64", 0.8)
     weapon_2 = Weapon("Hofner 500/1", 0.6)
     weapon_3 = Weapon("Squier VI", 0.4)
 
     weapon_4 = Weapon("Rickenbacker 330", 0.8)
     weapon_5 = Weapon("Fender Vintera 60s Mustang", 0.6)
     weapon_6 = Weapon("Gretsch 6122", 0.4)
-    print(test_weapon)
-    number_of_tests = 100
-    number_of_breaks = 0
 
-    # I'm testing this 100 times and keeping track of how many times it breaks
-    # for i in range(number_of_tests):
-    #     if some_weapon.does_break():
-    #         number_of_breaks += 1
-    # percentage = (number_of_breaks / number_of_tests) * 100
-    # print("The {} broke {}% of the time in {} tests!".format(some_weapon.weapon_name, round(percentage),
-    #       number_of_tests))
-
-    #Creating my Duelist objects
+    # Creating my Duelist objects
     bass_player = Duelist("Aki Mizuguchi", weapon_1, weapon_2, weapon_3)
     guitarist = Duelist("Yori Asanagi", weapon_4, weapon_5, weapon_6)
-    some_weapon = Weapon("Master Sword", 0.99)
-    another_weapon = Weapon("Kokiri Sword", 0.4)
-    a_final_weapon = Weapon("Biggoron's Sword", 0.75)
-    sample_duelist = Duelist("Link", some_weapon, another_weapon, a_final_weapon)
-    print(sample_duelist)
 
     # Testing the get_winner_of_duel_name method of the Duelist object 'bass_player' a few times
     number_of_duels = 10
 
-    # for duel_number in range(number_of_duels):
-    #     winner = bass_player.get_winner_of_duel_name(guitarist)
-    #     print("THE WINNER OF DUEL #{} IS {}!".format(duel_number + 1, winner), end="\n\n")
+    for duel_number in range(number_of_duels):
+        winner = bass_player.get_winner_of_duel_name(guitarist)
+        print("THE WINNER OF DUEL #{} IS {}!".format(duel_number + 1, winner), end="\n\n")
 
 # DO NOT WRITE CODE BELOW THIS LINE
 
