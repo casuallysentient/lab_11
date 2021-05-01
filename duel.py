67c9be6cd9f94f23b544681f23d4d56f39a3ee6e
Reilly <reilly.ford@gmail.com>
2021-05-01 11:55:17 -0400

    1.3

--- duel.py
+++ duel.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Weapon:
     def __init__(self, weapon_name, strength):
         self.weapon_name = weapon_name
@@ -5,6 +8,11 @@ class Weapon:
 
     def __str__(self):
         return "\'{weapon_name}\' Weapon object (strength: {strength}).".format(weapon_name=self.weapon_name, strength=self.strength)
+
+    def does_break(self):
+        if random.random() < (self.strength / 2):
+            return True
+        return False
 
 
 class Duelist:
@@ -17,7 +25,7 @@ def main():
     """
     # Creating my Weapon objects
     test_weapon = Weapon('naginata', 0.75)
-    weapon_1 = Weapon("Rickenbacker 4001c64", 0.8)
+    some_weapon = Weapon("Rickenbacker 4001c64", 0.6)
     weapon_2 = Weapon("Hofner 500/1", 0.6)
     weapon_3 = Weapon("Squier VI", 0.4)
 
@@ -25,6 +33,16 @@ def main():
     weapon_5 = Weapon("Fender Vintera 60s Mustang", 0.6)
     weapon_6 = Weapon("Gretsch 6122", 0.4)
     print(test_weapon)
+    number_of_tests = 100
+    number_of_breaks = 0
+
+    # I'm testing this 100 times and keeping track of how many times it breaks
+    for i in range(number_of_tests):
+        if some_weapon.does_break():
+            number_of_breaks += 1
+    percentage = (number_of_breaks / number_of_tests) * 100
+    print("The {} broke {}% of the time in {} tests!".format(some_weapon.weapon_name, round(percentage),
+          number_of_tests))
 
     # Creating my Duelist objects
     # bass_player = Duelist("Aki Mizuguchi", weapon_1, weapon_2, weapon_3)
